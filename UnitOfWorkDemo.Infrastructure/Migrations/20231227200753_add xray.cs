ca393545955b5fc706a346da0da0b25569e8d9f6
thisara-medi <thisarabandara.medi@gmail.com>
2023-12-28 01:38:39 +0530

    add xray migration

---
+++ UnitOfWorkDemo.Infrastructure/Migrations/20231227200753_add xray.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PMS.Infrastructure.Migrations
+{
+    public partial class addxray : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "xRayNo",
+                table: "PatientRecord",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "xRayNo",
+                table: "PatientRecord");
+        }
+    }
+}
